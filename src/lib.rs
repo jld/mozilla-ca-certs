46ed58c59e617af3c85f40b1bb3015fa4f959e57
Jed Davis <jld@mozilla.com>
2016-09-21 14:52:47 -0600

    Emitting the actual bits into the .o file might be better.

--- src/lib.rs
+++ src/lib.rs
@@ -9,33 +9,33 @@ mod autogen {
 pub use types::{Certificate, Trust, TrustLevel};
 
 /// (Documentation goes here.)
-pub const ALL_CERTIFICATES: &'static [Certificate<'static>] =
+pub static ALL_CERTIFICATES: &'static [Certificate<'static>] =
     autogen::ALL_CERTIFICATES;
 
 /// (Documentation goes here.)
-pub const ALL_TRUST_RECORDS: &'static [Trust<'static>] =
+pub static ALL_TRUST_RECORDS: &'static [Trust<'static>] =
     autogen::ALL_TRUST_RECORDS;
 
 /// (Documentation goes here.)
-pub const TLS_SERVER_TRUST_ROOTS: &'static [Certificate<'static>] =
+pub static TLS_SERVER_TRUST_ROOTS: &'static [Certificate<'static>] =
     autogen::TLS_SERVER_TRUST_ROOTS;
 
 /// (Documentation goes here.)
-pub const TLS_SERVER_DISTRUSTS: &'static [Trust<'static>] =
+pub static TLS_SERVER_DISTRUSTS: &'static [Trust<'static>] =
     autogen::TLS_SERVER_DISTRUSTS;
 
 /// (Documentation goes here.)
-pub const EMAIL_TRUST_ROOTS: &'static [Certificate<'static>] =
+pub static EMAIL_TRUST_ROOTS: &'static [Certificate<'static>] =
     autogen::EMAIL_TRUST_ROOTS;
 
 /// (Documentation goes here.)
-pub const EMAIL_DISTRUSTS: &'static [Trust<'static>] =
+pub static EMAIL_DISTRUSTS: &'static [Trust<'static>] =
     autogen::EMAIL_DISTRUSTS;
 
 /// (Documentation goes here.)
-pub const CODE_SIGNING_TRUST_ROOTS: &'static [Certificate<'static>] =
+pub static CODE_SIGNING_TRUST_ROOTS: &'static [Certificate<'static>] =
     autogen::CODE_SIGNING_TRUST_ROOTS;
 
 /// (Documentation goes here.)
-pub const CODE_SIGNING_DISTRUSTS: &'static [Trust<'static>] =
+pub static CODE_SIGNING_DISTRUSTS: &'static [Trust<'static>] =
     autogen::CODE_SIGNING_DISTRUSTS;
